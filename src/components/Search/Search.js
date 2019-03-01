d7ffcb2dac76f12c751bdaa3aa2e01821984c1e8
Krista Brehm <kristabrehm1@gmail.com>
2019-03-01 15:43:35 -0500

    added search input with icon

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-
 class SearchInput extends Component {
     render() {
         return (
-            <div class="SearchInput">
-
+            <div class="SearchContainer">
+                <input id="search" placeholder="Search"/>
+                <i class="material-icons">search</i>
             </div>
         );
     }
