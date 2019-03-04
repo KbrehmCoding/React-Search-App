f41118bd04b4e62e0fbeca5fafe3b89389faa8a5
Krista Brehm <kristabrehm1@gmail.com>
2019-03-03 23:27:47 -0500

    got the books api imported into the search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import routes from '../../../routes/api/google'
 // look at 3/28 class example to make sure search is hooked up to backend
 class SearchInput extends Component {
     state = {
         query: '',
+        results: []
     }
     handleInputChange = () => {
         this.setState({
             query: this.search.value
         })
     }
+    // TODO find way to use the routes export to connect search inpit to this input
+
     render() {
         return (
             <div class="SearchContainer">
                 <h6>Book Search</h6>
                 <form>
                     <label>Book</label>
-                    {/* TODO link google search to this input */}
                     <input id="search" placeholder="Search" type="text" ref={input => this.search = input}
                         onChange={this.handleInputChange} />
                 </form>
