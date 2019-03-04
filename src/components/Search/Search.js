91cfd27c6799f0414b9d0fdeecab6dc0d36657c1
Krista Brehm <kristabrehm1@gmail.com>
2019-03-03 23:15:50 -0500

    placed search input inside of a form tag, added js to handle the input change

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 // look at 3/28 class example to make sure search is hooked up to backend
 class SearchInput extends Component {
+    state = {
+        query: '',
+    }
+    handleInputChange = () => {
+        this.setState({
+            query: this.search.value
+        })
+    }
     render() {
         return (
             <div class="SearchContainer">
                 <h6>Book Search</h6>
-                <label>Book</label>
-                {/* TODO link google search to this input */}
-                <input id="search" placeholder="Search" type="text"/>
+                <form>
+                    <label>Book</label>
+                    {/* TODO link google search to this input */}
+                    <input id="search" placeholder="Search" type="text" ref={input => this.search = input}
+                        onChange={this.handleInputChange} />
+                </form>
                 <button>Search</button>
             </div>
         );
