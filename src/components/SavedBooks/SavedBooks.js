6ec007738e193f84e30705381d544ed1f57a0fc0
Krista Brehm <kristabrehm1@gmail.com>
2019-03-04 21:29:24 -0500

    tried to connect the book schema to the search results and saved books to populate the information on the books

--- src/components/SavedBooks/SavedBooks.js
+++ src/components/SavedBooks/SavedBooks.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import { Router, HashRouter} from "react-Router-dom";
+import { Router, HashRouter} from 'react-Router-dom';
 import { Card, CardContent } from 'react-materialize';
-import book from ".../models/book"
+import book from '../../../models/book'
+
 
 class SavedBooks extends Component {
     render() {
         return (
             <HashRouter>
                 <div class="SavedBooks">
-                    <Card className='small'>
+                    <Card className="small">
                         <CardContent>
                             <div>
                             {/* Need to look into how to correctly call the books api to this card */}
-                                <Router path=".../models/book">
-
+                                <Router path=".../models/index">
+                                    {book}
                                 </Router>
                             </div>
                             <button>Veiw</button>
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import { Card, CardContent } from 'react-materialize';
-import book from ".../models/book"
+import { Router, HashRouter} from "react-Router-dom";
+import book from '../../../models/book'
 
 class SearchResults extends Component {
     render() {
         return (
-            <div class="SearchResults">
-                <Card className='small'>Card Title
+            <HashRouter>
+                <div class="SearchResults">
+                    <Card className='small'>Card Title
                     <CardContent>
-                    {/* figure out the correct way to call this */}
-                        {book.title}
-                        {book.subtitle}
-                        {book.authors}
-                        {book.image} {book.description}
-                        <button>Save</button>
-                    </CardContent>
-                </Card>
-            </div>
+                            {/* figure out the correct way to call this */}
+                            <Router>
+                                {book}
+                            </Router>
+                            <button>Save</button>
+                        </CardContent>
+                    </Card>
+                </div>
+            </HashRouter>
         );
     }
 }
